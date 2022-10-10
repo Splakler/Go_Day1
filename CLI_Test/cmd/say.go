89fd390cb4a47856860de40140346877bf7f7fcb
brian (Linux MSI) <brian@brian-ferber.de>
2022-10-10 15:27:15 +0200

    added say und sayhello

---
+++ CLI_Test/cmd/say.go
@@ -0,0 +1,41 @@
+/*
+Copyright © 2022 NAME HERE <EMAIL ADDRESS>
+*/
+package cmd
+
+import (
+	"errors"
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+// sayCmd represents the say command
+var sayCmd = &cobra.Command{
+	Use:   "say",
+	Short: "A brief description of your command",
+	Long: `A longer description that spans multiple lines and likely contains examples
+and usage of using your command. For example:
+
+Cobra is a CLI library for Go that empowers applications.
+This application is a tool to generate the needed files
+to quickly create a Cobra application.`,
+	RunE: func(cmd *cobra.Command, args []string) error {
+		fmt.Println("say called")
+		return errors.New("provide item to the say command")
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(sayCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// sayCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// sayCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
