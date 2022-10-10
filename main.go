21dfbefb1d6d9e04bbf0b692249eda9e12677ed9
brian (Linux MSI) <brian@brian-ferber.de>
2022-10-10 11:45:08 +0200

    fixed main.go

--- main.go
+++ main.go
@@ -16,3 +16,7 @@ func main() {
 	fmt.Println(cap(arr))
 
 }
+
+func pointer(x_pt *[]int) {
+	*x_pt = append(*x_pt, 30000)
+}
--- pointers.go
+++ pointers.go
@@ -1,5 +1,21 @@
 package main
 
-func pointer(x_pt *[]int) {
+import "fmt"
+
+func main() {
+	var arr = []int{1, 2, 3}
+	var pt = &arr
+
+	fmt.Println(&pt)
+	fmt.Println(arr)
+	fmt.Println(cap(arr))
+	pointers(pt)
+	fmt.Println(&pt)
+	fmt.Println(arr)
+	fmt.Println(cap(arr))
+
+}
+
+func pointers(x_pt *[]int) {
 	*x_pt = append(*x_pt, 30000)
 }
